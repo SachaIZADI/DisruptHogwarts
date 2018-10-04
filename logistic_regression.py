852458c7327cd19042d162a26b541e23e92e4559
SachaIZADI <sacha.izadi@polytechnique.edu>
2018-10-04 16:25:39 +0200

    created the functions train and predict

--- logistic_regression.py
+++ logistic_regression.py
@@ -4,11 +4,10 @@ import math
 class LogisticRegression:
 
 
-    def __init__(self, X, y=None, train=True, path_to_beta=None, optimizer='gradient_descent', optimizer_params={'alpha':0.5,'n':100}):
+    def __init__(self, X, y=None, path_to_beta=None, optimizer='gradient_descent', optimizer_params={'alpha':0.5,'n':100}):
         self.X = X
         self.y = y
         self.unique_labels = list(set(y))
-        self.train = train
         self.optimizer = optimizer
         self.optimizer_params = optimizer_params
         if path_to_beta is None :
@@ -23,6 +22,10 @@ class LogisticRegression:
 
     def imputation(self):
         #impute missing values
+        return
+
+    def preprocessing(self):
+        #PCA / scaling
         return
 
 
@@ -65,38 +68,51 @@ class LogisticRegression:
 
 
 
-    def gradient(self):
-        m = self.X.shape[0]
+    def unit_gradient(self, x, y):
         gradient = {}
-
         for label in self.unique_labels:
-            gradient[label] = 0
-            for i in range(m):
-                probas = self.probabilities(self.X[i])
-                gradient[label] += self.X[i] * ((self.y[i]==label) - probas[label])
-            gradient[label] = -1/m * gradient[label]
-
+            probas = self.probabilities(x)
+            gradient[label] = x * ((y == label) - probas[label])
         return gradient
+
+
+    def full_gradient(self):
+        m = self.X.shape[0]
+        full_gradient = {}
+        for label in self.unique_labels:
+            full_gradient[label] = 0
+        for i in range(m):
+            unit_gradient = self.unit_gradient(self.X[i], self.y[i])
+            for label in self.unique_labels:
+                full_gradient[label] += unit_gradient[label]
+        for label in self.unique_labels:
+            full_gradient[label] = -1/m * full_gradient[label]
+        return full_gradient
 
 
 
     def gradient_descent(self, show_progress=True):
         params = self.optimizer_params
         for i in range(params['n']):
-            for label in self.unique_labels :
-                gradient = self.gradient()
-                self.beta[label] = self.beta[label] - params['alpha']*gradient[label]
+            for label in self.unique_labels:
+                full_gradient = self.full_gradient()
+                self.beta[label] = self.beta[label] - params['alpha']*full_gradient[label]
 
             if show_progress and i%20==0:
                 print("iteration n°%s - - - - - - - - - - - loss: %s" % (i, self.loss()))
-
-
+                # TODO : dynamic plotting of the optimization process
 
 
 
     def train(self):
-        return
+        if self.optimizer == 'gradient_descent':
+            self.gradient_descent()
+        #TODO : add other optimizers, SGD, Adam, Newton-Raphson
+        else:
+            return
 
-    def predict(self):
-        return
+
+    def predict(self,x):
+        probas = self.probabilities(x)
+        return max(probas, key=probas.get)
 
