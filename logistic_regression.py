0d2808fbd734171c5f36d28d655e827433854604
SachaIZADI <sacha.izadi@polytechnique.edu>
2018-10-04 10:37:48 +0200

    working on the logistic regression

---
+++ logistic_regression.py
@@ -0,0 +1,17 @@
+
+
+class LogisticRegression:
+
+    def __init__(self):
+        return
+
+    def loadDataSet(self):
+        return
+
+    def train(self):
+        return
+
+    def predict(self):
+        return
+
+    
